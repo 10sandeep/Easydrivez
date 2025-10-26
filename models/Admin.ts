3024e5bc11ef61c15998582b5a7ddf9dfa0193f7
10sandeep <sandeepnayak1424@gmail.com>
2025-10-27 01:48:16 +0530

    Fix mongoose import

--- models/Admin.ts
+++ models/Admin.ts
@@ -1,26 +1,26 @@
-// import mongoose, { Schema, Document, Model } from "mongoose";
+import mongoose, { Schema, Document, Model } from "mongoose";
 
 
-// interface IAdmin extends Document {
-//   username: string;
-//   email: string;
-//   password: string;
-//   img?: string;
-//   phone?: string;
-// }
+interface IAdmin extends Document {
+  username: string;
+  email: string;
+  password: string;
+  img?: string;
+  phone?: string;
+}
 
 
-// const AdminSchema = new Schema<IAdmin>({
-//   username: { type: String, required: true, unique: true },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   img: { type: String },
-//   phone: { type: String },
-// });
+const AdminSchema = new Schema<IAdmin>({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  img: { type: String },
+  phone: { type: String },
+});
 
 
-// const Admin: Model<IAdmin> =
-//   (mongoose.models.Admin as Model<IAdmin>) ||
-//   mongoose.model<IAdmin>("Admin", AdminSchema);
+const Admin: Model<IAdmin> =
+  (mongoose.models.Admin as Model<IAdmin>) ||
+  mongoose.model<IAdmin>("Admin", AdminSchema);
 
-// export default Admin;
+export default Admin;
