59249a0314b7865d90dd83de460b05a8ddcd3666
10sandeep <sandeepnayak1424@gmail.com>
2025-10-27 02:18:08 +0530

    Removed

--- lib/data.ts
+++ lib/data.ts
@@ -331,7 +331,7 @@ export const termsAndConditionsData = {
 
  export const services = [
     {
-      id: 'Rent car',
+      id: 'car',
       title: 'Rent A Car',
       description:
         'Booking a self-driving car with us is simple and easy. You can browse our selection of vehicles online, choose the car that best fits your needs, and book it for the duration of your choice. Our user-friendly platform allows you to manage your bookings and view your trip history with ease.',
@@ -340,7 +340,7 @@ export const termsAndConditionsData = {
       features: ['LUXURY', 'COMFORT', 'PROTECTION']
     },
     {
-      id: 'Rent bike',
+      id: 'bike',
       title: 'Rent A Bike',
       description:
         'Booking a self-driving bike with us is simple and easy. You can browse our selection of vehicles online, choose the bike that best fits your needs, and book it for the duration of your choice. Our user-friendly platform allows you to manage your bookings and view your trip history with ease.',
