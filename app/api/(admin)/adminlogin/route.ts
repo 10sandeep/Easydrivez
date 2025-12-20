0df11d55c401a72ccdc9e06886dc18b30995297e
msubham193 <msubham193@gmail.com>
2025-12-20 12:32:59 +0530

    done

--- app/api/(admin)/adminlogin/route.ts
+++ app/api/(admin)/adminlogin/route.ts
@@ -1,3 +1,7 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const runtime = "nodejs";
+
 import { NextResponse, NextRequest } from "next/server";
 import { adminLogin } from "@/repository/admin.repository";
 import { adminLoginValidator } from "@/validators/adminlogin.validator";
